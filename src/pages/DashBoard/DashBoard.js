09b6d89201349d7b213b64cccb62e48519c47214
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-05-26 17:01:27 +0600

    adnim panal working

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const DashBoard = () => {
+
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
+
     return (
         <div>
             <div class="drawer drawer-mobile">
@@ -16,10 +23,19 @@ const DashBoard = () => {
                     <label for="dashboard-sidebar" class="drawer-overlay"></label>
                     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
 
-                        <li><Link to='/dashboard'>My Items</Link></li>
-                        <li><Link to='/dashboard/reviews'>My Reviews</Link></li>
+                        {
+                            !admin && <> <li><Link to='/dashboard'>My Items</Link></li>
+                                <li><Link to='/dashboard/reviews'>My Reviews</Link></li></>
+                        }
                         <li><Link to='/dashboard/myprofile'>My Profile</Link></li>
-                        <li><Link to='/dashboard/allusers'>All Users</Link></li>
+
+                        {admin && <>
+                            <li><Link to='/dashboard/allusers'>All Users</Link></li>
+                            <li><Link to="/dashboard/addProduct">Add Product</Link></li>
+                            <li><Link to="/dashboard/manageallproducts">Manage All Products</Link></li>
+                            <li><Link to="/dashboard/manageallorders">Manage All Orders</Link></li>
+                        </>}
+
                     </ul>
 
                 </div>
