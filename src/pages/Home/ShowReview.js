6d1664aab87e7112f1a85ab1048cf57139c9205d
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-05-27 23:40:18 +0600

    ratings edited

--- src/pages/Home/ShowReview.js
+++ src/pages/Home/ShowReview.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import StarRatings from 'react-star-ratings/build/star-ratings';
 
 const ShowReview = ({ review }) => {
     const { email, ratings, descreption } = review;
@@ -8,9 +7,8 @@ const ShowReview = ({ review }) => {
             <div class="card w-full bg-base-100 shadow-xl">
                 <div class="card-body mx-auto">
                     <h2 class="card-title">{email}</h2>
-                    <p>Ratings : {ratings}</p>
-
-                    <p>{descreption}</p>
+                    <p>Ratings : {ratings} out of 5</p>
+                    <p>Comments : {descreption}</p>
                 </div>
             </div>
         </div >
